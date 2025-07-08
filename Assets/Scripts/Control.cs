6174901fbc0be0ac9ac9c480e47770cb63b8b789
Michael Lees <msle237.lees@gmail.com>
2025-07-07 20:15:15 -0400

    trying another machine

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using AustinHarris.JsonRpc;
+using TMPro;
 
 // Define the dataclass for the Sub position
 public class SubPos {
@@ -106,29 +107,52 @@ public class Control : MonoBehaviour
     public GameObject sub;
     public GameObject Gate;
     public GameObject Pole;
+    public GameObject COM;
+
+    public TMPro.TextMeshProUGUI X_pos;
+    public TMPro.TextMeshProUGUI Y_pos;
+    public TMPro.TextMeshProUGUI Z_pos;
+    public TMPro.TextMeshProUGUI Pitch_pos;
+    public TMPro.TextMeshProUGUI Yaw_pos;
+    public TMPro.TextMeshProUGUI Roll_pos;
+    public TMPro.TextMeshProUGUI X_vel;
+    public TMPro.TextMeshProUGUI Y_vel;
+    public TMPro.TextMeshProUGUI Z_vel;
+    public TMPro.TextMeshProUGUI Roll_vel;
+    public TMPro.TextMeshProUGUI Pitch_vel;
+    public TMPro.TextMeshProUGUI Yaw_vel;
+    public TMPro.TextMeshProUGUI Gate_distance;
+    public TMPro.TextMeshProUGUI Pole_distance;
+    public TMPro.TextMeshProUGUI Gate_angle;
+    public TMPro.TextMeshProUGUI Pole_angle;
 
     // Define the RPC methods for communication between unity sim and the python server
-    class Rpc : JsonRpcService {
+    class Rpc : JsonRpcService
+    {
         Control control;
 
-        public Rpc(Control control) {
+        public Rpc(Control control)
+        {
             this.control = control;
         }
 
         // Define the getSubPos method to get the Sub position within the simulation
         [JsonRpcMethod]
-        public SubPos getSubPos() {
+        public SubPos getSubPos()
+        {
             return new SubPos(control.sub.transform.position.x, control.sub.transform.position.y, control.sub.transform.position.z);
         }
 
         [JsonRpcMethod]
-        public SubRot getSubRot() {
+        public SubRot getSubRot()
+        {
             return new SubRot(control.sub.transform.rotation.eulerAngles.x, control.sub.transform.rotation.eulerAngles.y, control.sub.transform.rotation.eulerAngles.z);
         }
 
         // Define the getSubMeasuredVel method to get the Sub measured velocity within the simulation
         [JsonRpcMethod]
-        public SubMeasuredVel getSubMeasuredVel() {
+        public SubMeasuredVel getSubMeasuredVel()
+        {
             return new SubMeasuredVel(
                 control.sub.GetComponent<Rigidbody>().velocity.x,
                 control.sub.GetComponent<Rigidbody>().velocity.y,
@@ -141,31 +165,26 @@ public class Control : MonoBehaviour
 
         // Define the setSubSetVel method to set the Sub velocity within the simulation
         [JsonRpcMethod]
-        public void setSubSetVel(SubSetVel subSetVel) {
+        public void setSubSetVel(SubSetVel subSetVel)
+        {
             Vector3 linear = new Vector3(subSetVel.x, subSetVel.y, subSetVel.z);
 
             Debug.Log($"Received setSubSetVel: Linear={linear}, Yaw={subSetVel.yaw}");
 
-            // Move linearly in world space
-            control.sub.transform.position += linear * Time.deltaTime;
+            control.sub.transform.Translate(
+                linear * Time.deltaTime * 2 // move the sub in the direction of the linear velocity
+            );
 
-            // Rotate around the sub's own up axis (Yaw only)
-            if (Mathf.Abs(subSetVel.yaw) > 1e-3f) {
-                control.sub.transform.RotateAround(
-                    control.sub.transform.position,
-                    control.sub.transform.up,
-                    subSetVel.yaw * Time.deltaTime
-                );
-            }
-
-            // Stop physics interference (optional but recommended)
-            Rigidbody rb = control.sub.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+            control.sub.transform.RotateAround(
+                control.COM.transform.position,         // pivot point (Vector3)
+                control.sub.transform.up,               // axis of rotation (Vector3)
+                subSetVel.yaw                   // angle in degrees (float)
+            );
         }
 
         [JsonRpcMethod]
-        public Distance getDistance() {
+        public Distance getDistance()
+        {
             float gate_distance = Vector3.Distance(control.sub.transform.position, control.Gate.transform.position);
             float pole_distance = Vector3.Distance(control.sub.transform.position, control.Pole.transform.position);
             float gate_angle = Vector3.Angle(control.sub.transform.forward, control.Gate.transform.position - control.sub.transform.position);
@@ -175,7 +194,8 @@ public class Control : MonoBehaviour
         }
 
         [JsonRpcMethod]
-        public void restartPosition() {
+        public void restartPosition()
+        {
             control.restartPosition();
         }
     }
@@ -197,6 +217,40 @@ public class Control : MonoBehaviour
     void Update()
     {
         // sub.GetComponent<Rigidbody>().centerOfMass = new Vector3(sub.transform.position.x, sub.transform.position.y, sub.transform.position.z);
+        float x = sub.transform.position.x;
+        float y = sub.transform.position.y;
+        float z = sub.transform.position.z;
+        float pitch = sub.transform.rotation.eulerAngles.x;
+        float yaw = sub.transform.rotation.eulerAngles.y;
+        float roll = sub.transform.rotation.eulerAngles.z;
+        float x_vel = sub.GetComponent<Rigidbody>().velocity.x;
+        float y_vel = sub.GetComponent<Rigidbody>().velocity.y;
+        float z_vel = sub.GetComponent<Rigidbody>().velocity.z;
+        float roll_vel = sub.GetComponent<Rigidbody>().angularVelocity.x;
+        float pitch_vel = sub.GetComponent<Rigidbody>().angularVelocity.y;
+        float yaw_vel = sub.GetComponent<Rigidbody>().angularVelocity.z;
+        float gate_distance = Vector3.Distance(sub.transform.position, Gate.transform.position);
+        float pole_distance = Vector3.Distance(sub.transform.position, Pole.transform.position);
+        float gate_angle = Vector3.Angle(sub.transform.forward, Gate.transform.position - sub.transform.position);
+        float pole_angle = Vector3.Angle(sub.transform.forward, Pole.transform.position - sub.transform.position);
+
+        // Update the TextMeshPro objects with the current values
+        X_pos.text = "X: " + x.ToString("F2");
+        Y_pos.text = "Y: " + y.ToString("F2");
+        Z_pos.text = "Z: " + z.ToString("F2");
+        Pitch_pos.text = "Pitch: " + pitch.ToString("F2");
+        Yaw_pos.text = "Yaw: " + yaw.ToString("F2");
+        Roll_pos.text = "Roll: " + roll.ToString("F2");
+        X_vel.text = "X Vel: " + x_vel.ToString("F2");
+        Y_vel.text = "Y Vel: " + y_vel.ToString("F2");
+        Z_vel.text = "Z Vel: " + z_vel.ToString("F2");
+        Roll_vel.text = "Roll Vel: " + roll_vel.ToString("F2");
+        Pitch_vel.text = "Pitch Vel: " + pitch_vel.ToString("F2");
+        Yaw_vel.text = "Yaw Vel: " + yaw_vel.ToString("F2");
+        Gate_distance.text = "Gate Distance: " + gate_distance.ToString("F2");
+        Pole_distance.text = "Pole Distance: " + pole_distance.ToString("F2");
+        Gate_angle.text = "Gate Angle: " + gate_angle.ToString("F2");
+        Pole_angle.text = "Pole Angle: " + pole_angle.ToString("F2");
     }
 
     // Define the method to restart the Sub position
