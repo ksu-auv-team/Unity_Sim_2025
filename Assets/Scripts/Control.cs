0611b64e252bec77823ba869f6e9a14610ce9f4b
Michael Lees <msle237.lees@gmail.com>
2025-07-12 15:26:08 -0400

    built and modified and optimized

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -109,23 +109,6 @@ public class Control : MonoBehaviour
     public GameObject Pole;
     public GameObject COM;
 
-    public TMPro.TextMeshProUGUI X_pos;
-    public TMPro.TextMeshProUGUI Y_pos;
-    public TMPro.TextMeshProUGUI Z_pos;
-    public TMPro.TextMeshProUGUI Pitch_pos;
-    public TMPro.TextMeshProUGUI Yaw_pos;
-    public TMPro.TextMeshProUGUI Roll_pos;
-    public TMPro.TextMeshProUGUI X_vel;
-    public TMPro.TextMeshProUGUI Y_vel;
-    public TMPro.TextMeshProUGUI Z_vel;
-    public TMPro.TextMeshProUGUI Roll_vel;
-    public TMPro.TextMeshProUGUI Pitch_vel;
-    public TMPro.TextMeshProUGUI Yaw_vel;
-    public TMPro.TextMeshProUGUI Gate_distance;
-    public TMPro.TextMeshProUGUI Pole_distance;
-    public TMPro.TextMeshProUGUI Gate_angle;
-    public TMPro.TextMeshProUGUI Pole_angle;
-
     // Define the RPC methods for communication between unity sim and the python server
     class Rpc : JsonRpcService
     {
@@ -216,41 +199,6 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // sub.GetComponent<Rigidbody>().centerOfMass = new Vector3(sub.transform.position.x, sub.transform.position.y, sub.transform.position.z);
-        float x = sub.transform.position.x;
-        float y = sub.transform.position.y;
-        float z = sub.transform.position.z;
-        float pitch = sub.transform.rotation.eulerAngles.x;
-        float yaw = sub.transform.rotation.eulerAngles.y;
-        float roll = sub.transform.rotation.eulerAngles.z;
-        float x_vel = sub.GetComponent<Rigidbody>().velocity.x;
-        float y_vel = sub.GetComponent<Rigidbody>().velocity.y;
-        float z_vel = sub.GetComponent<Rigidbody>().velocity.z;
-        float roll_vel = sub.GetComponent<Rigidbody>().angularVelocity.x;
-        float pitch_vel = sub.GetComponent<Rigidbody>().angularVelocity.y;
-        float yaw_vel = sub.GetComponent<Rigidbody>().angularVelocity.z;
-        float gate_distance = Vector3.Distance(sub.transform.position, Gate.transform.position);
-        float pole_distance = Vector3.Distance(sub.transform.position, Pole.transform.position);
-        float gate_angle = Vector3.Angle(sub.transform.forward, Gate.transform.position - sub.transform.position);
-        float pole_angle = Vector3.Angle(sub.transform.forward, Pole.transform.position - sub.transform.position);
-
-        // Update the TextMeshPro objects with the current values
-        X_pos.text = "X: " + x.ToString("F2");
-        Y_pos.text = "Y: " + y.ToString("F2");
-        Z_pos.text = "Z: " + z.ToString("F2");
-        Pitch_pos.text = "Pitch: " + pitch.ToString("F2");
-        Yaw_pos.text = "Yaw: " + yaw.ToString("F2");
-        Roll_pos.text = "Roll: " + roll.ToString("F2");
-        X_vel.text = "X Vel: " + x_vel.ToString("F2");
-        Y_vel.text = "Y Vel: " + y_vel.ToString("F2");
-        Z_vel.text = "Z Vel: " + z_vel.ToString("F2");
-        Roll_vel.text = "Roll Vel: " + roll_vel.ToString("F2");
-        Pitch_vel.text = "Pitch Vel: " + pitch_vel.ToString("F2");
-        Yaw_vel.text = "Yaw Vel: " + yaw_vel.ToString("F2");
-        Gate_distance.text = "Gate Distance: " + gate_distance.ToString("F2");
-        Pole_distance.text = "Pole Distance: " + pole_distance.ToString("F2");
-        Gate_angle.text = "Gate Angle: " + gate_angle.ToString("F2");
-        Pole_angle.text = "Pole Angle: " + pole_angle.ToString("F2");
     }
 
     // Define the method to restart the Sub position
