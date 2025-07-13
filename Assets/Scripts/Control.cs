3449b6aaa81dd115576a70fc3a8a16c87bc9673c
msle237-lees <msle237.lees@gmail.com>
2025-07-13 14:04:19 -0400

    new argument allowing thing

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -165,16 +165,6 @@ public class Control : MonoBehaviour
         }
 
         [JsonRpcMethod]
-        public Distance getDistance() {
-            float gate_distance = Vector3.Distance(control.sub.transform.position, control.Gate.transform.position);
-            float pole_distance = Vector3.Distance(control.sub.transform.position, control.Pole.transform.position);
-            float gate_angle = Vector3.Angle(control.sub.transform.forward, control.Gate.transform.position - control.sub.transform.position);
-            float pole_angle = Vector3.Angle(control.sub.transform.forward, control.Pole.transform.position - control.sub.transform.position);
-
-            return new Distance(gate_distance, pole_distance, gate_angle, pole_angle);
-        }
-
-        [JsonRpcMethod]
         public void restartPosition() {
             control.restartPosition();
         }
@@ -185,6 +175,28 @@ public class Control : MonoBehaviour
     void Awake()
     {
         rpc = new Rpc(this);
+        string[] args = Environment.GetCommandLineArgs();
+
+        foreach (string arg in args)
+        {
+            Debug.Log("Arg: " + arg);
+
+            if (arg == "-headless")
+            {
+                Debug.Log("Running in headless mode");
+                // Set flags or disable rendering features here
+                QualitySettings.vSyncCount = 0;
+                Application.targetFrameRate = 60;
+                Screen.SetResolution(1920, 1080, false);
+
+                // Disable shaders and other rendering features and turn on batch mode
+                QualitySettings.shadows = ShadowQuality.Disable;
+                QualitySettings.shadowResolution = ShadowResolution.Low;
+                QualitySettings.antiAliasing = 0;
+                QualitySettings.softParticles = false;
+                Application.batchMode = true;
+            }
+        }
     }
 
     // Start is called before the first frame update
